6bbadac71f24257328e5c75fc594c2a7784d0aab
otis <pro100print@yahoo.com>
2017-05-26 11:51:58 +0300

    fix

--- ex/session/counter.php
+++ ex/session/counter.php
@@ -1,3 +1,8 @@
 <?php
 
 session_start();
+
+$counter =  (!isset($_SESSION['counter'])) ? 0 : $_SESSION['counter'];
+$counter++;
+$_SESSION['counter'] = $counter;
+echo "$counter просмотров";
--- ex/session/unset.php
+++ ex/session/unset.php
@@ -1,5 +1,20 @@
 <?php
+session_start();
 unset($_SESSION['date']); // Удаляет только дату
-session_destroy(); //Удаляет все данные внесенные пользователем
-
-
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <ul>            
+            <li><a href="index.php">Присвоить значение даты</a></li>
+            <li><a href="second.php">Используем присвоенное значение</a></li>
+            <li><a href="third.php">Еще раз его используем</a></li>
+            <li><a href="unset.php">Удаляем данные сессии</a></li>
+        </ul>
+    </body>
+</html>
